8cdab0b300f50eb8efa61b072d9096417d7b84fa
SpringSunYY <3217778348@qq.com>
2024-10-09 17:20:07 +0800

    前端初始化

---
+++ mianshiya-frontend/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
