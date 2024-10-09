8a966a961fb81357bd6337907409b95d73fe9de1
SpringSunYY <3217778348@qq.com>
2024-10-09 20:59:13 +0800

    完成前端

--- mianshiya-frontend/src/app/layout.tsx
+++ mianshiya-frontend/src/app/layout.tsx
@@ -1,5 +1,51 @@
+"use client";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import BasicLayout from "@/layouts/BasicLayout";
+import React, { useCallback, useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import store, { AppDispatch } from "@/stores";
+import { getLoginUserUsingGet } from "@/api/userController";
+import AccessLayout from "@/access/AccessLayout";
+import { setLoginUser } from "@/stores/loginUser";
 import "./globals.css";
+
+/**
+ * 全局初始化逻辑
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<
+  Readonly<{
+    children: React.ReactNode;
+  }>
+> = ({ children }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  // 初始化全局用户状态
+  const doInitLoginUser = useCallback(async () => {
+    const res = await getLoginUserUsingGet();
+    if (res.data) {
+      // 更新全局用户状态
+      dispatch(setLoginUser(res.data));
+    } else {
+      // 仅用于测试
+      // setTimeout(() => {
+      //   const testUser = {
+      //     userName: "测试登录",
+      //     id: 1,
+      //     userAvatar: "https://www.code-nav.cn/logo.png",
+      //     userRole: ACCESS_ENUM.ADMIN
+      //   };
+      //   dispatch(setLoginUser(testUser));
+      // }, 3000);
+    }
+  }, []);
+
+  // 只执行一次
+  useEffect(() => {
+    doInitLoginUser();
+  }, []);
+  return children;
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +53,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <body>
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          <Provider store={store}>
+            <InitLayout>
+              <BasicLayout>
+                <AccessLayout>{children}</AccessLayout>
+              </BasicLayout>
+            </InitLayout>
+          </Provider>
+        </AntdRegistry>
       </body>
     </html>
   );
